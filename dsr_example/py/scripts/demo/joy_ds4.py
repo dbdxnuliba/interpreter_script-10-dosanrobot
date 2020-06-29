61b2629f123b92b9fa81da1958eeae3695d94799
Wilder dyd <wilder@grupodyd.com>
2020-06-29 16:34:18 -0500

    Docuementación archivos en ingles

--- dsr_example/py/scripts/demo/joy_ds4.py
+++ dsr_example/py/scripts/demo/joy_ds4.py
@@ -30,7 +30,7 @@ def shutdown():
     pub_stop.publish(stop_mode=STOP_TYPE_QUICK)
     return 0
 
-""" Procesa los valores que llegan del nodo joy, y toma las decisiones de movimiento y control del gripper """
+""" Take the input from joy node and make decitions for move the robot and control the gripper"""
 def joy_cb(msg):
     global m_joyAnalogFlag
     global m_xyCompareFlag
@@ -42,22 +42,22 @@ def joy_cb(msg):
     homePosition = [0, 0, 0, 0, 0, 0]
 
     if msg.buttons[7] == 1 and msg.buttons[6] == 1:
-        r.movej(initialPosition, 50, 50) #posición inicial para movimiento del robot
+        r.movej(initialPosition, 50, 50) # Initial position for start move the robot
     elif msg.buttons[10] == 1:
-        r.movej(homePosition, 50, 50) #posición de no trabajo del robot
+        r.movej(homePosition, 50, 50) # Robot home position 
     
     if msg.buttons[0] == 1:
-        srv_robotiq_2f_move(0.4) #Recoger objeto corresponde al radio del objeto (X)
+        srv_robotiq_2f_move(0.4) # Pick the object, close the gripper as far as object ratio(X)
         moveObject = True
 
     if msg.buttons[2] == 1:
-        srv_robotiq_2f_move(0) #abrir pinza (TRIANGULO)
+        srv_robotiq_2f_move(0) # Open the gripper (Triangle)
         moveObject = False
     
     if msg.buttons[1] == 1 and not moveObject:
-        srv_robotiq_2f_move(0.8) #cerrar pinza (O)
+        srv_robotiq_2f_move(0.8) # Close the gripper (O)
     
-    #Activación de banderas según el eje del movimiento, depende de los joystick del control
+    # Enable or disable flags according to axis movement
     if msg.axes[4] != 0 or msg.axes[0] != 0 or msg.axes[1] != 0:
         m_joyAnalogFlag = True
     else:
@@ -69,7 +69,7 @@ def joy_cb(msg):
         else:
             m_xyCompareFlag = True
     
-    # se asignan velocidades y dirección del movimiento
+    # assign movement velocity and direction
     if m_joyAnalogFlag and m_joyJogFlag == -1:
         if msg.axes[4] > 0: #z+
             m_joyJogFlag = JOG_AXIS_TASK_Z 
@@ -90,16 +90,14 @@ def joy_cb(msg):
             m_joyJogFlag = JOG_AXIS_TASK_Y
             m_joyJogVel = 20
         r.jog(m_joyJogFlag, COORDINATE_SYSTEM_TOOL, m_joyJogVel)
-    else: #Condición cuando se presiona algún botón o se mueve un joystick en alguna dirección no especificada
+    else: # Condition when the input is not definded
         if not m_joyAnalogFlag:
             r.jog(m_joyJogFlag, COORDINATE_SYSTEM_TOOL, 0)
             m_joyJogFlag = -1
 
 def dh(d, theta, a, alpha):
     """
-    Calcular la matriz de transformacion homogenea asociada con los parametros
-    de Denavit-Hartenberg.
-    Los valores d, theta, a, alpha son escalares.
+    Calculated the homogenous transformation matrix according to Denavit-Hartenberg parameters.
     """
     cti = np.cos(theta)
     cai = np.cos(alpha)
@@ -113,19 +111,15 @@ def dh(d, theta, a, alpha):
 
 def fkine_ur5(q):
     """
-    Calcular la cinematica directa del robot UR5 dados sus valores articulares. 
-    q es un vector numpy de la forma [q1, q2, q3, q4, q5, q6
+    Calculated direct cinematic of the robot according to its articulates parameters [q1, q2, q3, q4, q5, q6]
     """
-    ### Recibe q en arreglo tipo columna de 6x1
-    # Matrices DH (completar)
     T1 = dh(0.1525, q[0], 0, -pi/2)
     T2 = dh(0, q[1]-pi/2, 0.62, 0)
     T3 = dh(0.0345, q[2]+pi/2, 0, pi/2)
     T4 = dh(0.559, q[3]+pi, 0, pi/2)
     T5 = dh(0, q[4]+pi, 0, pi/2)
     T6 = dh(0.121, q[5], 0, 0)
-    
-    # Efector final con respecto a la base
+
     T = np.dot(np.dot(np.dot(np.dot(np.dot(T1, T2), T3), T4), T5), T6)
     return T
 
@@ -143,18 +137,18 @@ def meters2milimiters(pos):
 r = CDsrRobot(ROBOT_ID, ROBOT_MODEL)
 pi=np.pi
 moveObject = False
-bmarker_des = BallMarker(color['BLUE']) # Color AZUL de la esfera como objecto a manipular
-pub_stop = rospy.Publisher('/'+ROBOT_ID +ROBOT_MODEL+'/stop', RobotStop, queue_size=10) #Detener la comunicación con el robot
-sub_joy  = rospy.Subscriber("joy", Joy, joy_cb) #Captuar cuando el control de Ps4 es manipulado
-srv_robotiq_2f_move = rospy.ServiceProxy('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/robotiq_2f_move', Robotiq2FMove) #manipular la pinza
+bmarker_des = BallMarker(color['BLUE']) # Object
+pub_stop = rospy.Publisher('/'+ROBOT_ID +ROBOT_MODEL+'/stop', RobotStop, queue_size=10) # Stop comunication with the robot
+sub_joy  = rospy.Subscriber("joy", Joy, joy_cb)
+srv_robotiq_2f_move = rospy.ServiceProxy('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/robotiq_2f_move', Robotiq2FMove) # manipulated the gripper
 if __name__ == "__main__":
     rospy.init_node('joy_ps4_py') 
     rospy.on_shutdown(shutdown)
 
-    srv_robotiq_2f_move(0.8) #cerrar pinza
-    r.movej([0, 0, 0, 0, 0, 0], 50, 50) #llevar el robot a la posición de home
-    pd = np.array([0.5, 0.5, 0.5]) # Posicion inicial de la esfera AZUL 
-    bmarker_des.xyz([0.5, 0.5, 0.5]) # Donde tiene que llegar
+    srv_robotiq_2f_move(0.8) # Close the gripper
+    r.movej([0, 0, 0, 0, 0, 0], 50, 50) # Robot home position
+    pd = np.array([0.5, 0.5, 0.5]) # Initial position of the object 
+    bmarker_des.xyz(pd) # draw object 
     rate = rospy.Rate(500)
     while not rospy.is_shutdown():
         if moveObject:
